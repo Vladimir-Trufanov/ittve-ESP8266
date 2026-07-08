8fb22ab90cb1439540e7a630bc51bd79f96c94fb
Труфанов Владимир Евгеньевич <44765119+Vladimir-Trufanov@users.noreply.github.com>
2026-07-08 21:30:26 +0300

    Выделен спрайт будущего  класса

--- ESP-NOW/receiver_CYD_minimum/spriteMain.h
+++ ESP-NOW/receiver_CYD_minimum/spriteMain.h
@@ -11,20 +11,27 @@
 #include <Arduino.h>
 #include <TFT_eSPI.h>
 
+char* IntToChar(uint16_t numbIn); 
+
 class TSprite_Main 
 {
   public:
 
-  // Построить объект (конструктор класса)
   TSprite_Main(int iQueueSize=4); 
-
+  void init(); 
+  void sayLine(int16_t x, int16_t y, char* line); 
+  void say(); 
   void clrScr(bool color); 
-
-
 
   private:
 
-  int t;
+  TFT_eSPI tft = TFT_eSPI();
+  TFT_eSprite stext3 = TFT_eSprite(&tft); 
+
+  uint16_t i=0;
+  char line[320];  // строка спрайта
+  char fill[320];  // заполнитель строки
+
 
   /*
   // Определяем счетчик прерываний от таймера и общее их количество до
@@ -42,8 +49,11 @@ class TSprite_Main
 
   TSprite_Main::TSprite_Main(int iQueueSize) 
   {
-    TFT_eSPI tft = TFT_eSPI();
-    t=iQueueSize;
+    // Чистим заполнитель
+    memset(fill,32,319); 
+    fill[319]='\0';
+    //
+    int t=iQueueSize;
   };
 
   void TSprite_Main::clrScr(bool color) 
@@ -51,5 +61,68 @@ class TSprite_Main
     bool c=color;
   }
 
+  void TSprite_Main::init() 
+  {
+    // Задаем среднюю глубину цвета и создаем спрайт
+    // заданного размера в пикселах в оперативной памяти
+    stext3.setColorDepth(8);
+    stext3.createSprite(200, 110);
+    // Заполняем буфер памяти, выделенный под спрайт, заданным цветом
+    stext3.fillSprite(TFT_BLACK);
+    // Отключаем перенос текста и по горизонтали и по вертикали 
+    stext3.setTextWrap(false, false);
+    // Определяем цвет текста с прозрачным фоном  
+    stext3.setTextColor(TFT_WHITE,TFT_BLACK,true); 
+    // Загружаем шрифт в память спрайта
+    //stext3.loadFont("HuaweiSans16");     // загрузка в память шрифта
+    stext3.loadFont("nasalization48");     
+  }
+  
+  void TSprite_Main::sayLine(int16_t x, int16_t y, char* line) 
+  {
+    // Устанавливаем позицию курсора для вывода текста в заданной позиции спрайта
+    stext3.setCursor(x,y);
+    stext3.print(fill);
+    stext3.setCursor(x,y);
+    stext3.print(line); 
+  }
+
+  // Так как предполагается вывод всего 2 строк, то 
+  // вначале выводим прежнее значение в нижней части
+  void TSprite_Main::say() 
+  {
+    char chi[6]="Чи";
+    char exclam[2]="!";
+
+    memset(line,'\0',320); 
+    strcat(line,chi); 
+    strcat(line,IntToChar(i)); 
+    strcat(line,exclam); 
+    sayLine(10,55,line);
+
+    // Выше складываем новое значение
+    i++;
+    memset(line,'\0',320); 
+    strcat(line,chi); 
+    strcat(line,IntToChar(i)); 
+    strcat(line,exclam); 
+    sayLine(10,10,line);
+    
+    // Выталкмваем спрайт на дисплей с заданной позиции
+    stext3.pushSprite(10,60);
+  }
+
+  // ****************************************************************************
+  // *            Преобразовать беззнаковое  целое в строку символов            *
+  // ****************************************************************************
+  char charNumby[6]; // char[5]+'\0'
+  char* IntToChar(uint16_t numbIn) 
+  {
+    uint16_t numby=numbIn;
+    memset(charNumby,'\0',6); 
+    if (numby>65534) numby=0;
+    String(numby).toCharArray(charNumby,6);
+    return charNumby;
+  }
 
   // *********************************************************** spriteMain.h ***
